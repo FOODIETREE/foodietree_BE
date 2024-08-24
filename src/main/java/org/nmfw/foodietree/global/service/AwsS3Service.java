e4fcb3cf3d9b88ccab09cf9a4bb16b317e57027c
Maybaba <sinyunjong@gmail.com>
2024-08-24 13:11:04 +0900

    s3 setitngs awsS3Service

--- src/main/java/org/nmfw/foodietree/global/service/AwsS3Service.java
+++ src/main/java/org/nmfw/foodietree/global/service/AwsS3Service.java
@@ -3,7 +3,12 @@ package org.nmfw.foodietree.global.service;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+
+import javax.annotation.PostConstruct;
 
 @Service
 @Slf4j
@@ -20,6 +25,19 @@ public class AwsS3Service {
     private String region;
     @Value("${aws.bucketName}")
     private String bucketName;
+
+    // 사용자접근 인증
+    @PostConstruct void initAmazonS3() {
+        //액세스 키와 비밀키로 사용자 인증
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        this.s3 = S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+    }
+
+
 
 
 }
